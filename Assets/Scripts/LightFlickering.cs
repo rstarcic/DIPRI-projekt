76186508abce030f99f09fc8b76a8b5461852189
rstarcic <roberta.starcic589@gmail.com>
2024-04-26 01:20:55 +0200

    drugi commit

--- Assets/Scripts/LightFlickering.cs
+++ Assets/Scripts/LightFlickering.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LightFlickering : MonoBehaviour
 {
     public Light light;
     public float minTime;
