4332b1cc7ef30ec79a306e51af8713ce558ceafd
Elvis <estaric@unipu.hr>
2024-05-02 19:00:00 +0200

    Added second floor hallway, and a script to move from one scene to the other

---
+++ Assets/Scripts/MoveToScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToScene : MonoBehaviour
+{
+    public void Move(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+}
