336938f9628141672e0e436ecfcbe4c704aaf3f7
Elvis <estaric@unipu.hr>
2024-06-01 18:37:06 +0200

    Movement script updated to work with detective

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,12 @@ public class MovementScript : MonoBehaviour
 {
     private NavMeshAgent agent;
     public Camera cam1;
+    Animator playerAnim;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        playerAnim = GetComponent<Animator>();
         
     }
 
@@ -24,8 +26,22 @@ public class MovementScript : MonoBehaviour
             if(Physics.Raycast(ray, out hit))
             {
                 agent.SetDestination(hit.point);
+                print("Walk");
+                playerAnim.SetBool("Walk", true);
             }
+    
         }
+       if (!agent.pathPending)
+{
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                   playerAnim.SetBool("Walk", false);
+                }
+            }
+}
+
         if(Input.GetMouseButton(0))
         {
          cam1.transform.RotateAround(agent.nextPosition, 
