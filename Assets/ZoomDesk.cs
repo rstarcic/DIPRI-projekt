999fba7d5692c43930ffae1abd3d544a0d8fd64c
rstarcic <roberta.starcic589@gmail.com>
2024-06-11 22:28:32 +0200

    Zooming on desk and pinboard

---
+++ Assets/ZoomDesk.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomDesk : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Camera zoomCamera;
+
+    void Start()
+    {
+        if (mainCamera == null)
+        {
+            Debug.LogError("Main camera reference is not set!");
+        }
+        if (zoomCamera == null)
+        {
+            Debug.LogError("Zoom camera reference is not set!");
+        }
+
+        if (zoomCamera != null)
+        {
+            zoomCamera.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag("Desk"))
+                {
+                    Debug.Log("Clicked on the desk!");
+                    if (mainCamera != null && zoomCamera != null)
+                    {
+                        mainCamera.enabled = false;
+                        zoomCamera.enabled = true;
+                    }
+                }
+                else
+                {
+                    Debug.Log("Clicked outside the desk!");
+                    if (mainCamera != null && zoomCamera != null)
+                    {
+                        zoomCamera.enabled = false;
+                        mainCamera.enabled = true;
+                    }
+                }
+            }
+        }
+    }
+}
