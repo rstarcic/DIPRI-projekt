1c46c8406599b4c045954a707fdf31a1dc1e1943
Elvis <estaric@unipu.hr>
2024-06-17 14:23:51 +0200

    Created items; Added items scripts to gameObjects; Edited room enter script to check for keys in inventory; Added message to display that the player does not have rquired key to enter the room

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -1,13 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 public class ButtonScript : MonoBehaviour
 {
     public int SceneId;
+    public bool requresKey;
+    public int keyid;
+    public GameObject text = null;
+    public void Start()
+    {
+        if(text!=null)
+        {
+            text.SetActive(false);
+        }
+    }
     
    private void OnMouseDown()
     {
-        gameObject.GetComponent<MoveToScene>().Move(SceneId);
+        if(requresKey==false)
+        {
+            gameObject.GetComponent<MoveToScene>().Move(SceneId);
+        }
+        else if(requresKey==true)
+        {
+            bool hasNoKey = true;
+            InvManager InvManager= GameObject.FindAnyObjectByType<InvManager>();
+            List<Item> itemsInInv = InvManager.Items;
+            foreach(Item it in itemsInInv)
+            {
+                if(it.id==keyid)
+                {
+                    gameObject.GetComponent<MoveToScene>().Move(SceneId);
+                    hasNoKey = false;
+                }
+            }
+            if(hasNoKey==true)
+                {
+                    text.SetActive(true);
+                    Invoke("disableMessage",3f);
+                }
+                
+        }
+    }
+    public void disableMessage()
+    {
+         text.SetActive(false);
     }
 }
