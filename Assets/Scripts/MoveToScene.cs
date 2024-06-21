7da636e4a129c6ad36b1f0cd0f15a619c461988a
rstarcic <roberta.starcic589@gmail.com>
2024-06-21 02:23:04 +0200

    added audio to doors, item key update, move to scene script update

--- Assets/Scripts/MoveToScene.cs
+++ Assets/Scripts/MoveToScene.cs
@@ -5,13 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MoveToScene : MonoBehaviour
 {
-   
     public Animator fade;
+    public AudioSource audioSource; 
+    public AudioClip doorSound;
 
     public void Move(int sceneID)
     {
         PlayerPrefs.SetInt("previousScene", SceneManager.GetActiveScene().buildIndex);
         fade.SetTrigger("SceneChanged");
-        SceneManager.LoadScene(sceneID,LoadSceneMode.Single);
+        StartCoroutine(PlaySoundAndLoadScene(sceneID));
+    }
+    IEnumerator PlaySoundAndLoadScene(int sceneID)
+    {
+        audioSource.clip = doorSound;
+        audioSource.Play();
+        yield return new WaitForSeconds(audioSource.clip.length); 
+        SceneManager.LoadScene(sceneID, LoadSceneMode.Single);
     }
 }
