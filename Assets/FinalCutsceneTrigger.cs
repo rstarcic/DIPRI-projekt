1d5b988098c06b261cdaa6a6347fe2f58fc6d1d9
Luka <benkovi.luka3@gmail.com>
2024-07-07 21:17:00 +0200

    final final cutscene

--- Assets/FinalCutsceneTrigger.cs
+++ Assets/FinalCutsceneTrigger.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class FinalCutsceneTrigger : MonoBehaviour
 {
@@ -26,5 +28,11 @@ public class FinalCutsceneTrigger : MonoBehaviour
             }
         // Activate the cutscene GameObject after the delay
         _cutscene.SetActive(true);
+            StartCoroutine(BackToMainMenu(51.0f));
+    }
+    private IEnumerator BackToMainMenu(float delay) {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(0);
+
     }
 }
